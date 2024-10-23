1c810c65144429b55587e8754adcedb0974f233a
divya16chaudhary <136861181+divya16chaudhary@users.noreply.github.com>
2024-10-23 20:59:38 +0530

    commit messag is url

--- src/components/Job/JobDetails.jsx
+++ src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://jobhub-1-4sae.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
